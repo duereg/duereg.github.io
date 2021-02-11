fd71a52aeb132cedfea89a14bee35c04171bc3ec
Matt Blair <mblair@slack-corp.com>
2021-02-10 22:36:35 -0800

    newest version

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  $(function() {
-    $("#australian-vs-us-coffee-terms-comparison table").addClass("table-striped");
-    return $("img").addClass("img-responsive");
-  });
-
-}).call(this);
